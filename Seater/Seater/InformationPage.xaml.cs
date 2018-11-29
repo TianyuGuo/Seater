d9c0a3782b0e4008356cbc8376245d4430ebef92
Erick Salas <esalas.ua@gmail.com>
2018-11-28 19:14:02 -0500

    Add files via upload

---
+++ Seater/Seater/InformationPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace Seater
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InformationPage : ContentPage
+    {
+       // public InformationPage()
+       /// {
+        //    InitializeComponent();
+        //}
+
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SearchPage());
+        }
+        private void Button_Clicked_search(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SearchPage());
+        }
+
+        private void Button_Clicked_reserve(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ReservationPage1());
+        }
+
+        private void Button_Clicked_profile(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ProfilePage());
+        }
+    }
+}
